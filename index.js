1e1e3067d55533ef84c57be413ca9e865df67c40
WesleyAugusto <ranzerap@gmail.com>
2022-09-21 23:26:35 -0300

    let var const

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-console.log("Hello world!");
-console.log("Hello world!");
-console.log("Hello world!");
-//console.log("Hello world!"); para fazer um comentario linha unica
-/**
- * data do comentario 
- * como 
- * onde é chamado
- * como foi feito 
- * obs:comentario mais detalhado :
- */
+var text = "Hello, World!"; 
+
+const connection = "http://localhost:" //constant nao pode ser alteradas
+
+    console.log("Olá Mundo!");
+    console.log (text);
+    console.log("Olá Mundo!!");
+    console.log("Olá Mundo!!");
+
+    if (2==2){
+                //let por escopo, por um treixo de codigo    
+        let calc = 3
+        console.log(calc);
+
+        console.log (text);
+    }
