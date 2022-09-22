36e232c5f2399ab0310107d955f1b04489ec7d99
WesleyAugusto <ranzerap@gmail.com>
2022-09-21 23:12:56 -0300

    como fazer um comentario js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log("Hello world!");
 console.log("Hello world!");
 console.log("Hello world!");
+//console.log("Hello world!"); para fazer um comentario linha unica
+/**
+ * data do comentario 
+ * como 
+ * onde é chamado
+ * como foi feito 
+ * obs:comentario mais detalhado :
+ */
