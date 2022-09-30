0411b0c78865c332e2aa69aaaadbef4f63ec8b49
WesleyAugusto <ranzerap@gmail.com>
2022-09-29 22:46:43 -0300

    arrays forEach

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-let now = Date.now (); //timestamp
+//arrays
 
-console.log ('%c ${now}','color: white; background-color: blue');
+var montadoras = ["vw"," fiat"," ford"," honda"," new Date()","true"," function(){}]; 
 
+console.log(montadoras.length);
+console.log(montadoras[3]);
+console.log(montadoras[4].getDate());
+console.log(montadoras[4].toLocalDateString("pt-Br"));
+console.log(montadoras[6](1,1,1,));
+//voce pode pegar atribultos de uma função ou de  um obejeto 
 
+//maneira de  percorrer os arrays forEach
 
-
-let now = new Date (); 
-
-console.log('%c ${now.getDate()}','color: white; background-color: blue'); //dia
-console.log('%c ${now.getMonth()}','color: white; background-color: blue');//mês
-console.log('%c ${now.getFullYear()}','color: white; background-color: blue');//ano
-console.log('%c ${now.toLocaleDateString(pt-BR)}','color: white; background-color: blue');//tras o horario de acordo com pais de  origem da  pessoa
-
-
+montadoras.forEach(function(value, index){
+    console.log(value, index)
+})
