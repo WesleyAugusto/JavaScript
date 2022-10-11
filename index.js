51a56dcf8631a656c4ab09158f21aa6011c4d0c1
WesleyAugusto <ranzerap@gmail.com>
2022-10-10 23:12:31 -0300

    orientada objeto

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-//arrays
+let celular = function(){
+    this.cor = "preto";
 
-var montadoras = ["vw"," fiat"," ford"," honda"," new Date()","true"," function(){}]; 
+    this.ligar = function(){
+        console.log("ligando!")
+        return "ok"
+    }
+}
 
-console.log(montadoras.length);
-console.log(montadoras[3]);
-console.log(montadoras[4].getDate());
-console.log(montadoras[4].toLocalDateString("pt-Br"));
-console.log(montadoras[6](1,1,1,));
-//voce pode pegar atribultos de uma função ou de  um obejeto 
+let objeto = new celular ();  
 
-//maneira de  percorrer os arrays forEach
+console.log (objeto.ligar());
 
-montadoras.forEach(function(value, index){
-    console.log(value, index)
-})
+//para chamar um metado dentro do objeto  usando ponto nome mais ()
+//funçoes e metados sempre terá que retornar algo
+
+
---
+++ index2.js
@@ -0,0 +1,11 @@
+class celular {
+    constructor() {
+        this.cor = "preto ";
+    }
+    ligar() {
+        console.log("ligando")
+        return "ok"
+    }
+}
+ let objeto = new celular();
+ console.log(objeto);
