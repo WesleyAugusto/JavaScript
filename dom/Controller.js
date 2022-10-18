992ee92e669d383b6c3a89589d74b1d6efe7b3d6
WesleyAugusto <ranzerap@gmail.com>
2022-10-18 12:50:22 -0300

    SetInterval e Date

--- dom/Controller.js
+++ dom/Controller.js
@@ -1,20 +1,30 @@
 class Controller {
     constructor(){
-        this._textElement = document.querySelector("#text");
+        this._timeElement = document.querySelector("#date");
         this.initialize();
     }
 initialize(){
     
-       this._textElement.innerHTML = "Titulo da pagina"
+    setInterval(() => {
+        this._timeElement.innerHTML = this.currentDate.toLocaleTimeString("pt-BR");
+    },1000)
+       
 
 }
 
 get textElement(){
-    return this._textElement;
+    return this._timeElement;
 }
 set textElement(value){
 
-    this._textElement.innerHTML = value;
+    this._timeElement.innerHTML = value;
 }
+
+get currentDate(){
+
+    return new Date();
+
+}
+
 
 }
